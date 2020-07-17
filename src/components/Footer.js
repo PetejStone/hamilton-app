384cb8a60c8f3d21a0a698df363df7a7c3a98cea
PetejStone <petejstoney@gmail.com>
2020-07-16 17:41:44 -0700

    added toml

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Header.scss'
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; {  new Date().getFullYear() } StoneSquared Development</p>
+            <p>&copy; {  new Date().getFullYear() } <a href="http://www.stonesquareddevelopment.com" target="_blank">StoneSquared Development</a></p>
         </footer>
     )
 }
